6ae8514613684555feea25a2a3de8f894379e5bb
Chris L <chris.ladden@gmail.com>
2022-06-10 13:47:14 -0500

    Added ACF's to homepage layout

---
+++ resources/views/front-page.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.frontPage')
+
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+  <?php if( have_rows('hp_flexible_content') ): ?>
+  <?php while( have_rows('hp_flexible_content') ): the_row(); ?>
+      <?php if( get_row_layout() == 'home_page_hero' ): ?>
+          @include('flexible-content.home_page_hero')
+      <?php endif; ?>
+       <?php if( get_row_layout() == 'one_col' ): ?>
+          @include('flexible-content.one-col')
+      <?php endif; ?>
+      <?php if( get_row_layout() == 'three_col' ): ?>
+          @include('flexible-content.three-col')
+      <?php endif; ?>
+      <?php if( get_row_layout() == 'listings' ): ?>
+          @include('flexible-content.listings')
+      <?php endif; ?>
+      <?php if( get_row_layout() == 'contact' ): ?>
+      @include('flexible-content.contact')
+  <?php endif; ?>
+  <?php endwhile; ?>
+<?php endif; ?>
+
+  @endwhile
+@endsection
--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -1,12 +1,13 @@
 {{--
-  Template Name: Custom Template
+  Template Name: Home Page Template
 --}}
 
-@extends('layouts.app')
+@extends('layouts.frontpage')
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
     @include('partials.content-page')
+    @include('partials.footer')
   @endwhile
 @endsection
