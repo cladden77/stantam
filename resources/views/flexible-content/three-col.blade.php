09cd5ae5edd4ebf5e7b29c72a549d46d5fe79691
Chris L <chris.ladden@gmail.com>
2022-06-12 14:11:10 -0500

    built and styled three-col section

--- resources/views/flexible-content/three-col.blade.php
+++ resources/views/flexible-content/three-col.blade.php
@@ -1,30 +1,33 @@
 <section class="three-col" id="@sub('three_col_id')">
     <div class="container p-5">
         <div class="row justify-content-center">
-            <h2>@sub('three_col_header')</h2>
-            <h3>@sub('three_col_subheader')</h3>
+            <h2 class="text-center">@sub('three_col_header')</h2>
+            <h3 class="text-center">@sub('three_col_subheader')</h3>
         </div>
-        <div class="row row-cols-1 row-cols-md-2 justify-content-center">
+        <div class="row justify-content-center mt-5">
             <div class="card-deck">
-            <div class="card mx-2" style="width: 18rem;">
+            <div class="card mx-2">
                 <div class="card-body text-center">
-                    <h5 class="card-title">@sub('three_col_left_card_title')</h5>
+                    <h4 class="card-title my-4">@sub('three_col_left_card_title')</h4>
                     <p class="card-text">@sub('three_col_left_card_content')</p>
-                    <a class="button button--primary" href="@sub('three_col_left_card_cta', 'url')">@sub('three_col_left_card_cta', 'title')</a>
+                    <div class="card-footer"><a class="button button--primary" href="@sub('three_col_left_card_cta', 'url')">@sub('three_col_left_card_cta', 'title')</a>
+                    </div>
                 </div>
             </div>
-            <div class="card mx-2" style="width: 18rem;">
+            <div class="card mx-2">
                 <div class="card-body text-center">
-                    <h5 class="card-title">@sub('three_col_middle_card_title')</h5>
+                    <h4 class="card-title my-4">@sub('three_col_middle_card_title')</h4>
                     <p class="card-text">@sub('three_col_middle_card_content')</p>
-                    <a class="button button--primary" href="@sub('three_col_middle_card_cta', 'url')">@sub('three_col_middle_card_cta', 'title')</a>
+                    <div class="card-footer"><a class="button button--primary" href="@sub('three_col_middle_card_cta', 'url')">@sub('three_col_middle_card_cta', 'title')</a>
+                    </div>
                 </div>
             </div>
-            <div class="card mx-2" style="width: 18rem;">
+            <div class="card mx-2">
                 <div class="card-body text-center">
-                    <h5 class="card-title">@sub('three_col_right_card_title')</h5>
+                    <h4 class="card-title my-4">@sub('three_col_right_card_title')</h4>
                     <p class="card-text">@sub('three_col_right_card_content')</p>
-                    <a class="button button--primary" href="@sub('three_col_right_card_cta', 'url')">@sub('three_col_right_card_cta', 'title')</a>
+                    <div class="card-footer"><a class="button button--primary" href="@sub('three_col_right_card_cta', 'url')">@sub('three_col_right_card_cta', 'title')</a>
+                    </div>
                 </div>
             </div>
          </div>
