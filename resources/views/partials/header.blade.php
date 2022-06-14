9825ce1f58a572726662223f42623fc6a722e6fd
Chris L <chris.ladden@gmail.com>
2022-06-14 16:26:35 -0500

    adjusted hamburger menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,24 +1,21 @@
 <header class="banner">
   <nav class="navbar">
-  <div class="container-fluid py-3 px-5">
-    <div class="col-6">
+  <div class="container-fluid py-3 px-0 px-md-5 px-sm-0">
+    <div class="col-8">
       <a class="navbar-brand" href="{{ home_url('/') }}"><img src="http://stentam.local/app/uploads/2022/06/Stentam-logo.png" alt="Stenson Tamaddon" /></a>
     </div>
 
-    <div class="col-6 text-right">
-      <div class="pos-f-t">
-        <div class="collapse" id="navbarToggleExternalContent">
+        <div class="collapse" id="navbarCollapse">
           <div class="bg-dark p-4">
             @if (has_nav_menu('primary_navigation'))
             {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
             @endif
           </div>
         </div>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="#navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-      </div>
-    </div>
+
   </div>
 </nav>
 </header>
