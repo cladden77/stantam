e76c82ef28185ba2ff860ff139916597c0f8366e
Chris L <chris.ladden@gmail.com>
2022-06-16 21:00:08 -0500

    stylized page header block

--- resources/views/flexible-content/page-hero.blade.php
+++ resources/views/flexible-content/page-hero.blade.php
@@ -1,6 +1,7 @@
-<section class="page-hero container-fluid">
-    <div class="row align-items-center pl-0 pl-lg-5 pl-md-0 hp-hero__content-wrapper my-auto">
-        <div class="text-center text-md-left text-sm-center px-5 py-5">
+<section class="page-hero container-fluid p-0">
+    <div class="row align-items-center page-hero__background" style="background: url('@sub('page_hero_image', 'url')') center center/cover no-repeat;">
+    <div class="pl-0 pl-lg-5 pl-md-0 page-hero__content-wrapper my-auto pt-5">
+        <div class="col-12 col-md-10 col-sm-12 text-center text-md-left text-sm-center px-5 pt-5">
             @hassub('hero_header')
             <h1>@sub('hero_header')</h1>
             @endsub
@@ -9,4 +10,5 @@
             @endsub
         </div>
     </div>
+</div>
 </section>
