241522a8c3da51431ecdb35dc18ae01b9174b818
Chris L <chris.ladden@gmail.com>
2022-06-20 14:16:44 -0500

    fixing formatting and functionality of navigation

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header class="banner">
-  <nav class="navbar">
+  {{-- <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark navbar-offcanvas">
   <div class="container-fluid py-3 px-0 px-md-5 px-sm-0">
     <div class="col-8">
       <a class="navbar-brand" href="{{ home_url('/') }}"><img src="http://stentam.local/app/uploads/2022/06/Stentam-logo.png" alt="Stenson Tamaddon" /></a>
@@ -17,5 +17,32 @@
           </button>
 
   </div>
+</nav> --}}
+
+<!--  START NAVBAR   -->
+<nav class="navbar navbar-expand-lg fixed-top navbar-offcanvas">
+  <div class="container-fluid py-3 px-0 px-md-5 px-sm-0">
+  <a class="navbar-brand" href="{{ home_url('/') }}"><img src="http://stentam.local/app/uploads/2022/06/Stentam-logo.png" alt="Stenson Tamaddon" /></a>
+  
+<!--     If you want to show the button online on mobile use this toggle   
+  
+  <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+-->
+  
+<!--      This toggle button is always visible  -->
+ <button class="navbar-toggler d-block" type="button" id="navToggle">
+  <span class="navbar-toggler-icon"></span>
+</button>
+
+  <div class="navbar-collapse offcanvas-collapse">
+    @if (has_nav_menu('primary_navigation'))
+            {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+            @endif
+  </div>
+  </div>
 </nav>
+<!--  END NAVBAR   -->
 </header>
