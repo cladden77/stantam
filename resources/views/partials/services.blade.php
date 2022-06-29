60dd07eea0d50f5ca03f100dbd6360263d72dcb2
Chris L <chris.ladden@gmail.com>
2022-06-29 08:03:19 -0500

    created services page template

---
+++ resources/views/partials/services.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.services')
+
+@section('services_content')
+  @while(have_posts()) @php the_post() @endphp
+  <?php if( have_rows('flexible_content') ): ?>
+  <?php while( have_rows('flexible_content') ): the_row(); ?>
+      <?php if( get_row_layout() == 'page_hero' ): ?>
+          @include('flexible-content.hp-hero')
+      <?php endif; ?>
+      <?php if( get_row_layout() == 'three_col' ): ?>
+          @include('flexible-content.three-col')
+      <?php endif; ?>
+      <?php if( get_row_layout() == 'info_slider' ): ?>
+          @include('flexible-content.info-slider')
+      <?php endif; ?>
+      <?php if( get_row_layout() == 'satisfaction_block' ): ?>
+          @include('flexible-content.satisfaction')
+      <?php endif; ?>
+      <?php if( get_row_layout() == 'full_width_cta' ): ?>
+      @include('flexible-content.full-width-cta')
+      <?php endif; ?>
+      <?php if( get_row_layout() == 'logo_slider' ): ?>
+      @include('flexible-content.logo-slider')
+      <?php endif; ?>
+      <?php if( get_row_layout() == 'recent_post_cards' ): ?>
+      @include('flexible-content.recent-post-cards')
+      <?php endif; ?>
+  <?php endwhile; ?>
+<?php endif; ?>
+
+  @endwhile
+@endsection
