9da0e62a69b4f4aec4ae47787a16cc03afa74f4b
miljov6 <milija.jovicic@gmail.com>
2022-07-04 01:42:29 +0200

    changes for all pages

--- resources/views/partials/job-openings.blade.php
+++ resources/views/partials/job-openings.blade.php
@@ -3,33 +3,82 @@
     <div class="container">
         <div class="filter">
             <h3>Filter by:</h3>
-            <select id='position'>
-                @fields('jobs')
-                <option value="@sub('position')">@sub('position')</option>
-                @endfields
-            </select>
             <select id='location'>
+                <option value="all_locations" selected>Location</option>
                 @fields('jobs')
                 <option value="@sub('location')">@sub('location')</option>
                 @endfields
             </select>
             <select id='team'>
+                <option value="all_teams" selected>Team</option>
                 @fields('jobs')
                 <option value="@sub('team')">@sub('team')</option>
                 @endfields
             </select>
-        </div>
-        <div>
-            <h3>@sub('title')</h3>
+            <select id='position'>
+                <option value="all_positions" selected>Position</option>
+                @fields('jobs')
+                <option value="@sub('position')">@sub('position')</option>
+                @endfields
+            </select>
         </div>
         <div class="jobs">
+            <div>
+                <h3>@sub('title')</h3>
+            </div>
             @fields('jobs')
-            <div class="job" data-position='@sub(' position')' data-location='@sub(' location')' data-team='@sub(' team')'>
+            <div class="job" data-position="@sub('position')" data-location="@sub('location')" data-team="@sub('team')">
                 <h4>@sub('job_title')</h4>
-                <button class="job_btn"><a href="@sub('job_link')">APPLY</a></button>
+                <div class="job_btn"><a href="@sub('job_link')"></a><span>APPLY</span></div>
             </div>
             @endfields
         </div>
     </div>
 </div>
 @endgroup
+<script>
+    // Location Filter
+
+    jQuery('#location').change(function(){
+        var location1 = jQuery(this).children('option:selected').val();
+        jQuery('.job').each(function() {
+            if (location1 == 'all_locations') {
+                jQuery(this).show();
+            } else {
+                if (jQuery(this).attr('data-location') != location1) {
+                jQuery(this).hide();
+            } else {
+                jQuery(this).show();
+            }
+            }
+        })
+    })
+
+
+    //Position Filter
+
+    jQuery('#position').change(function(){
+        var location1 = jQuery(this).children('option:selected').val();
+        jQuery('.job').each(function() {
+            if (jQuery(this).attr('data-position') != location1) {
+                jQuery(this).hide();
+            } else {
+                jQuery(this).show();
+            }
+        })
+    })
+
+
+    //Team Filter
+
+    jQuery('#team').change(function(){
+        var location1 = jQuery(this).children('option:selected').val();
+        jQuery('.job').each(function() {
+            if (jQuery(this).attr('data-team') != location1) {
+                jQuery(this).hide();
+            } else {
+                jQuery(this).show();
+            }
+        })
+    })
+</script>
--- resources/views/partials/our-culture.blade.php
+++ resources/views/partials/our-culture.blade.php
@@ -1,12 +1,24 @@
 @group('our_culture')
 <div class="our_culture">
-    <h3>@sub('title')</h3>
-    <div class="values row">
-        <div class="value col-12 col-md-6 col-sm-12">
-            @sub('text_on_left')
-        </div>
-        <div class="value col-12 col-md-6 col-sm-12">
-            @sub('text_on_right')
+    <div class="container">
+        <h2>@sub('title')</h2>
+        <div class="values row">
+            <div class="value col-12 col-md-6 col-sm-12">
+                @fields('text_on_left_repeat')
+                <div>
+                    <h4>@sub('title_of_box')</h4>
+                    <p>@sub('text_of_the_box')</p>
+                </div>
+                @endfields
+            </div>
+            <div class="value col-12 col-md-6 col-sm-12">
+                @fields('text_on_right_repeat')
+                <div>
+                    <h4>@sub('title_of_box')</h4>
+                    <p>@sub('text_of_the_box')</p>
+                </div>
+                @endfields
+            </div>
         </div>
     </div>
 </div>
