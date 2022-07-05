23d0467894baaba730d5cb6d9748f82de22f1a02
Chris L <chris.ladden@gmail.com>
2022-07-05 10:15:32 -0500

    styling sucess-stories block

--- resources/views/flexible-content/success-stories.blade.php
+++ resources/views/flexible-content/success-stories.blade.php
@@ -14,13 +14,18 @@
             <?php if( have_rows('success_story') ): ?>
             <div class="row no-gutters">
             <?php while( have_rows('success_story') ): the_row(); ?>
-            <div class="col card-group">
-                <div class="card img-fluid">
-                    <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
-                    <div class="card-img-overlay">
-                      <h4 class="card-title text-center">@sub('title')</h4>
-                    </div>
-                  </div></div>
+              <div class="col card-group">
+                  <div class="card img-fluid">
+                      <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+                      <div class="card-img-overlay">
+                        <h4 class="card-title text-center">@sub('title')</h4>
+
+                        <div class="mb-3 text-center">
+                          <a class="button button--primary" href="@sub('button', 'url')">@sub('button', 'title')</a>
+                        </div>
+                      </div>
+                  </div>
+              </div>
             <?php endwhile; ?>
             </div>
             <?php endif; ?>
