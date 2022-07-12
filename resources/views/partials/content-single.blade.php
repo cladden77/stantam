9de52fbb591d6e66f9bdd36541f21911da9bbd56
Chris L <chris.ladden@gmail.com>
2022-07-12 14:26:22 -0500

    formatted single post page layout and styling

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,13 +1,40 @@
+
 <article @php post_class() @endphp>
-  <header>
-    <h1 class="entry-title">{!! get_the_title() !!}</h1>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-content">
-    @php the_content() @endphp
+  <div class="container">
+    <div class="row">
+      <header>
+        <h1 class="entry-title">{!! get_the_title() !!}</h1>
+        @include('partials/entry-meta')
+      </header>
+    </div>
+
+    <div class="row">
+      <div class="sidebar col-3 d-none d-lg-block text-left mt-5 pl-0">
+        <p class="sidebar-title">Topics in this article:</p>
+        @fields('sidebar_links')
+        @hassub('link_title')
+          <p>@hassub('link')
+            <a href="@sub('link')">
+            @sub('link_title')
+            @endsub
+            </a>
+          </p>
+        @endsub
+        @endfields
+      </div>
+  
+      <div class="col-12 col-lg-9">
+        <div class="entry-content mt-5">
+          @php the_content() @endphp
+        </div>
+      </div>
+    </div>
   </div>
-  <footer>
-    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-  </footer>
-  @php comments_template('/partials/comments.blade.php') @endphp
+  
+  <div class="row">
+    <footer>
+      {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+    </footer>
+    {{-- @php comments_template('/partials/comments.blade.php') @endphp --}}
+  </div>
 </article>
--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,6 +1,8 @@
-<time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
-<p class="byline author vcard">
-  {{ __('By', 'sage') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
+<div class="d-inline-flex">
+<time class="updated pr-2" datetime="{{ get_post_time('c', true) }}">Published: {{ get_the_date() }}</time> |
+<p class="byline author vcard pl-2">
+  {{ __('Author:', 'sage') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
     {{ get_the_author() }}
   </a>
 </p>
+</div>
