d02585cf63b4b73ac340c166ce461297593d730d
Chris L <chris.ladden@gmail.com>
2022-07-11 22:13:02 -0500

    fixed job listing layout

--- resources/views/partials/job-openings.blade.php
+++ resources/views/partials/job-openings.blade.php
@@ -1,59 +1,61 @@
 @group('job_openings')
-<div class="job_openings">
+<div class="job_openings pt-5">
     <div class="container">
-        <div class="filter">
-            <h3>Filter by:</h3>
-            <select id='location'>
-                <option value="all_locations" selected>All Locations</option>
-                <?php if (have_rows('locations')) :
-                    $sub_values = array();
-                    while (have_rows('locations')) : the_row();
-                        array_push($sub_values, get_sub_field('location'));
-                    endwhile;
-                    $sub_values = array_unique($sub_values);
-                    foreach ($sub_values as $value) {
-                        echo "<option value='$value'>" . $value . "</option>";
-                    }
-                endif; ?>
-
-            </select>
-            <select id='team'>
-                <option value="all_teams" selected>All Teams</option>
-                <?php if (have_rows('jobs')) :
-                    $sub_values = array();
-                    while (have_rows('jobs')) : the_row();
-                        array_push($sub_values, get_sub_field('team'));
-                    endwhile;
-                    $sub_values = array_unique($sub_values);
-                    foreach ($sub_values as $value) {
-                        echo "<option value='$value'>" . $value . "</option>";
-                    }
-                endif; ?>
-            </select>
-            <select id='position'>
-                <option value="all_positions" selected>All Positions</option>
-                <?php if (have_rows('jobs')) :
-                    $sub_values = array();
-                    while (have_rows('jobs')) : the_row();
-                        array_push($sub_values, get_sub_field('position'));
-                    endwhile;
-                    $sub_values = array_unique($sub_values);
-                    foreach ($sub_values as $value) {
-                        echo "<option value='$value'>" . $value . "</option>";
-                    }
-                endif; ?>
-            </select>
+        <div class="row">
+            <div class="col-12 text-center text-md-left">
+                <div class="filter">
+                    <h3>Filter by:</h3>
+                    <select id='location'>
+                        <option value="all_locations" selected>All Locations</option>
+                        <?php if (have_rows('locations')) :
+                            $sub_values = array();
+                            while (have_rows('locations')) : the_row();
+                                array_push($sub_values, get_sub_field('location'));
+                            endwhile;
+                            $sub_values = array_unique($sub_values);
+                            foreach ($sub_values as $value) {
+                                echo "<option value='$value'>" . $value . "</option>";
+                            }
+                        endif; ?>
+                    </select>
+                    <select id='team'>
+                        <option value="all_teams" selected>All Teams</option>
+                        <?php if (have_rows('jobs')) :
+                            $sub_values = array();
+                            while (have_rows('jobs')) : the_row();
+                                array_push($sub_values, get_sub_field('team'));
+                            endwhile;
+                            $sub_values = array_unique($sub_values);
+                            foreach ($sub_values as $value) {
+                                echo "<option value='$value'>" . $value . "</option>";
+                            }
+                        endif; ?>
+                    </select>
+                    <select id='position'>
+                        <option value="all_positions" selected>All Positions</option>
+                        <?php if (have_rows('jobs')) :
+                            $sub_values = array();
+                            while (have_rows('jobs')) : the_row();
+                                array_push($sub_values, get_sub_field('position'));
+                            endwhile;
+                            $sub_values = array_unique($sub_values);
+                            foreach ($sub_values as $value) {
+                                echo "<option value='$value'>" . $value . "</option>";
+                            }
+                        endif; ?>
+                    </select>
+                </div>
+            </div>
         </div>
-        <div class="jobs">
-            <div>
+        <div class="row align-items-center justify-content-center justify-content-md-between justify-content-sm-center jobs pt-5">
                 <h3>@sub('title')</h3>
-            </div>
-            @fields('jobs')
-            <div class="job" data-position="@sub('position')" data-location="@sub('location')" data-team="@sub('team')">
-                <a href="@sub('job_link')"><h4>@sub('job_title')</h4></a>
-                <div class="job_btn"><a href="@sub('job_link')"></a><span>APPLY</span></div>
-            </div>
-            @endfields
+
+                @fields('jobs')
+                <div class="job" data-position="@sub('position')" data-location="@sub('location')" data-team="@sub('team')">
+                    <a href="@sub('job_link')"><h4>@sub('job_title')</h4></a>
+                    <div class="job_btn"><a href="@sub('job_link')"></a><span>APPLY</span></div>
+                </div>
+                @endfields
         </div>
     </div>
 </div>
