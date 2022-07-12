4aaf0d82f28dbeae045bb7f9394b683e31b3e320
Chris L <chris.ladden@gmail.com>
2022-07-11 21:42:58 -0500

    fixed layout issues in connect block

--- resources/views/partials/contact-content.blade.php
+++ resources/views/partials/contact-content.blade.php
@@ -24,55 +24,88 @@
 @endgroup
 @group('connect')
 <div class="connect">
-    @hassub('section_2_title')
-    <h2>@sub('section_2_title')</h2>
-    @endsub
-    @hassub('working_time')
-    <p>@sub('working_time')</p>
-    @endsub
-    <div>
-        @hassub('phone_icon')
-        <img src="@sub('phone_icon', 'url')" alt="@sub('phone_icon', 'alt')">
-        @endsub
-        @hassub('phone_text')
-        <p>@sub('phone_text')</p>
-        @endsub
-    </div>
-    <div>
-        @hassub('email_icon')
-        <img src="@sub('email_icon', 'url')" alt="@sub('email_icon', 'alt')">
-        @endsub
-        @hassub('email_text')
-        <p>@sub('email_text')</p>
-        @endsub
-    </div>
-    <div>
-        @hassub('address_icon')
-        <img src="@sub('address_icon', 'url')" alt="@sub('address_icon', 'alt')">
-        @endsub
-        @hassub('address_text')
-        <p>@sub('address_text')</p>
-        @endsub
-    </div>
-    <div>
-        @hassub('follow_text')
-        <p>@sub('follow_text')</p>
-        @endsub
-        @hassub('linkedin_icon')
-        <a href="@sub('linkedin_profile')">
-            <img src="@sub('linkedin_icon', 'url')" alt="@sub('linkedin_icon', 'alt')">
-        </a>
-        @endsub
-        @hassub('facebook_icon')
-        <a href="@field('facebook_profile')">
-            <img src="@sub('facebook_icon', 'url')" alt="@sub('facebook_icon', 'alt')">
-        </a>
-        @endsub
-        @hassub('twitter_icon')
-        <a href="@sub('twitter_profile')">
-            <img src="@sub('twitter_icon', 'url')" alt="@sub('twitter_icon', 'alt')">
-        </a>
-        @endsub
+    <div class="container">
+        <div class="row justify-content-center px-5">
+            <div class="col-12 text-center py-3">
+                @hassub('section_2_title')
+                <h2>@sub('section_2_title')</h2>
+                @endsub
+            </div>
+        </div>
+
+        <div class="row justify-content-center pb-3">
+            <div class="col-12 text-center">
+                <div class="d-inline-flex align-items-center">
+                    @hassub('working_time')
+                <p>@sub('working_time')</p>
+                @endsub
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-12 text-center">
+                <div class="d-inline-flex align-items-start">
+                    @hassub('phone_icon')
+                    <img src="@sub('phone_icon', 'url')" alt="@sub('phone_icon', 'alt')">
+                    @endsub
+                    @hassub('phone_text')
+                    <p>@sub('phone_text')</p>
+                    @endsub
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-12 text-center">
+                <div class="d-inline-flex align-items-center">
+                    @hassub('email_icon')
+                    <img src="@sub('email_icon', 'url')" alt="@sub('email_icon', 'alt')">
+                    @endsub
+                    @hassub('email_text')
+                    <p>@sub('email_text')</p>
+                    @endsub
+                </div>
+            </div>
+        </div>
+            
+        <div class="row justify-content-center">
+            <div class="col-12 text-center">
+                <div class="d-inline-flex align-items-center">
+                    @hassub('address_icon')
+                    <img src="@sub('address_icon', 'url')" alt="@sub('address_icon', 'alt')">
+                    @endsub
+                    @hassub('address_text')
+                    <p>@sub('address_text')</p>
+                    @endsub
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-12 text-center pb-5">
+                <div class="d-inline-flex align-items-center">
+                    @hassub('follow_text')
+                    <p class="pr-3">@sub('follow_text')</p>
+                    @endsub
+                    @hassub('linkedin_icon')
+                    <a href="@sub('linkedin_profile')">
+                        <img src="@sub('linkedin_icon', 'url')" alt="@sub('linkedin_icon', 'alt')">
+                    </a>
+                    @endsub
+                    @hassub('facebook_icon')
+                    <a href="@field('facebook_profile')">
+                        <img src="@sub('facebook_icon', 'url')" alt="@sub('facebook_icon', 'alt')">
+                    </a>
+                    @endsub
+                    @hassub('twitter_icon')
+                    <a href="@sub('twitter_profile')">
+                        <img src="@sub('twitter_icon', 'url')" alt="@sub('twitter_icon', 'alt')">
+                    </a>
+                    @endsub
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endgroup
