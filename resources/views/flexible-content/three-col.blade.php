502c61fe5d3b385426964476ce8ab2cd33b6f856
Chris L <chris.ladden@gmail.com>
2022-07-13 16:27:44 -0500

    made buttons optional

--- resources/views/flexible-content/three-col.blade.php
+++ resources/views/flexible-content/three-col.blade.php
@@ -11,24 +11,30 @@
                     <h4 class="card-title my-4">@sub('three_col_left_card_title')</h4>
                     <p class="card-text">@sub('three_col_left_card_content')</p>
                 </div>
+                @hassub('three_col_middle_card_cta')
                 <div class="card-footer text-center mb-4"><a class="button button--primary" href="@sub('three_col_left_card_cta', 'url')">@sub('three_col_left_card_cta', 'title')</a>
                 </div>
+                @endsub
             </div>
             <div class="card mx-2">
                 <div class="card-body text-center">
                     <h4 class="card-title my-4">@sub('three_col_middle_card_title')</h4>
                     <p class="card-text">@sub('three_col_middle_card_content')</p>
                 </div>
+                @hassub('three_col_middle_card_cta')
                 <div class="card-footer text-center mb-4"><a class="button button--primary" href="@sub('three_col_middle_card_cta', 'url')">@sub('three_col_middle_card_cta', 'title')</a>
                 </div>
+                @endsub
             </div>
             <div class="card mx-2">
                 <div class="card-body text-center">
                     <h4 class="card-title my-4">@sub('three_col_right_card_title')</h4>
                     <p class="card-text">@sub('three_col_right_card_content')</p>
                 </div>
+                @hassub('three_col_middle_card_cta')
                 <div class="card-footer text-center mb-4"><a class="button button--primary" href="@sub('three_col_right_card_cta', 'url')">@sub('three_col_right_card_cta', 'title')</a>
                 </div>
+                @endsub
             </div>
          </div>
     </div>
