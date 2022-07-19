ad47c77b325478712aeb422f51bac19580a44bb8
Chris L <chris.ladden@gmail.com>
2022-07-19 18:25:15 -0500

    made image optional

--- resources/views/flexible-content/full-width-content.blade.php
+++ resources/views/flexible-content/full-width-content.blade.php
@@ -2,8 +2,9 @@
     <div class="container py-5">
         <div class="row">
             <div class="col-12">
+                @hassub('image')
                 <img class="float-none float-md-left float-sm-none" src="@sub('image', 'url')" alt="@sub('image', 'alt')" />
-
+                @endsub
                 <div>
                 @hassub('header')
                 <h2 class="pb-4">@sub('header')</h2>
