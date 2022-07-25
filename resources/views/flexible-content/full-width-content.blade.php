9ea6b5204ff7b03cdd95ff6e7d006f4cacc02071
Chris L <chris.ladden@gmail.com>
2022-07-25 12:40:38 -0500

    adjusted vertical spacing

--- resources/views/flexible-content/full-width-content.blade.php
+++ resources/views/flexible-content/full-width-content.blade.php
@@ -1,5 +1,5 @@
-<section class="full-width-content pt-5">
-    <div class="container py-5">
+<section class="full-width-content mt-3">
+    <div class="container pt-5">
         <div class="row">
             <div class="col-12">
                 @hassub('image')
