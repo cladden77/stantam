328e6dcfda8d0a512add941037d345a1db9b3da3
Chris L <chris.ladden@gmail.com>
2022-07-28 08:36:22 -0500

    fixed contact header section

--- resources/views/partials/title-head.blade.php
+++ resources/views/partials/title-head.blade.php
@@ -1,4 +1,4 @@
-<div class="header container-fluid p-0">
+<div class="contact-header container-fluid p-0">
     <div class="row align-items-center header__background" style="background: url(@field('header_background_image', 'url')) center center/cover no-repeat;">
         <div class="pl-0 pl-lg-5 pl-md-0 my-auto pt-5">
             <div class="col-12 col-md-10 col-sm-12 text-center text-md-left text-sm-center px-5 pt-5">
