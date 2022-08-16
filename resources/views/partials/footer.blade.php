707da6d8df9dc1cb51c295f7665f3af0e0e2eefc
Chris L <chris.ladden@gmail.com>
2022-08-15 17:06:46 -0700

    tracking for the the BDR program

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -72,4 +72,11 @@
       </div>
     </div>
   </div>
+  <script type="text/javascript">
+    (function(i,s,o,g,r,a,m){i['SLScoutObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://scout-cdn.salesloft.com/sl.js','slscout');
+    slscout(["init", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0IjoxMDc1NDZ9.LgZLm7fLhEPzud4Tzul0PqHcCbDpHO5siSZPLjdDM8I"]);
+</script>
 </footer>
